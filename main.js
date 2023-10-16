a45e6d49a9a5b7fbcc8d81ab233bc68e904a2f22
Okechukwu Egeruoh <39450490+Chukwu12@users.noreply.github.com>
2023-10-16 14:20:29 +0000

    update

--- main.js
+++ main.js
@@ -233,3 +233,35 @@ for(let i = 0; i < pag.length; i++) {
     shoe = i + 1;
   });
 }
+
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close')
+
+
+
+if(navToggle) {
+  navToggle.addEventListener('click', ()=>{
+    navMenu.classList.add('show-menu')
+  })
+}
+// ================hidden menu=======================//
+if(navClose){
+  navClose.addEventListener('click', ()=>{
+    navMenu.classList.add('show-menu')
+  })
+}
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click',linkAction))
+
+/*=============== SWIPER SHOE ===============*/
+
+
+/*=============== SHADOW HEADER ===============*/
