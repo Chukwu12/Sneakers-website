562f701feadf4b0e8a9b8f9f7ab450c459584211
okey E <okeysmooed@gmail.com>
2023-10-17 16:14:39 +0000

    update

--- main.js
+++ main.js
@@ -266,10 +266,24 @@ let swiperShoes = new Swiper('.banner__swiper', {
   loop: true,
   spaceBetween: 32,
   grabCursor: true,
+  effect: 'creative',
+  creativeEffect: {
+    prev:{
+      translate: [-100, 0, -500],
+      opacity: 0,
+    },
+  },
+
+  next:{
+    translate: [-100, 0, -500],
+    opacity: 0,
+  },
+},
+
 
   pagination: {
     el: '.swiper-pagination',
-    clickable:true,
+    clickable: true,
   },
 })
 
